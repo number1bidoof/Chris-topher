22ffc27130885529435be62c95c8bdb1bd50ffa4
number1bidoof <anabdalla1@my.waketech.edu>
2025-04-29 12:48:23 -0400

    started psuedocode
    Co-authored-by: SirGuamsalot <Gcc07@users.noreply.github.com>

---
+++ psuedocode.js
@@ -0,0 +1,22 @@
+// Global Varribles
+const classes = ("Ninja","Knight","Wizard","Monk");
+const items = ("swords", "potions", "stickI","bomb", "compass", "dirt", "money", "ice");
+let user;
+let stats = {"Strength":0,"Speed":0,"Intlligence":0,"Defense":0};
+let aspects = new Array; //Will add late inside the first screen
+let userItems; // Displays the users items
+let userName = prompt("What is you name fair traveler? "); 
+// First Screen: Charater Customization
+function firstScreen() {
+    let spinor; //define spinor into varrible 
+    while(user != null) {
+        // charater customization sequence
+    }
+    if (user.Strength > 5){
+        // Display the are strong
+    } else {
+        // Display they are not st
+    }
+
+
+}
